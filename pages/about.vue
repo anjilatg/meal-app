9264f6c0649713a5a581f55a9ac4457d8ccd91f8
anjilatg <ngelatamang948@gmail.com>
2023-06-25 11:40:35 +0545

    about page modified

--- pages/about.vue
+++ pages/about.vue
@@ -10,7 +10,9 @@
 
 <style scoped>
 .box{
-    background-image:url('~assets/images/logo.png') ;
+    background-image: url("/assets/images/logo.png");
+    background-repeat: no-repeat;
+    background-position: center;
     height: 100vh;
     width: 100%;
 }
