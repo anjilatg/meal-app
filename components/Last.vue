b5cd77fce54f6158fb25d82c4a32d56769840ff7
anjilatg <ngelatamang948@gmail.com>
2023-07-05 09:31:38 +0545

    .

--- components/Last.vue
+++ components/Last.vue
@@ -1,21 +1,25 @@
 <template>
     <div class="w-full h-250px bg-yellow-950 py-5">
         <div class="grid grid-cols-15 container m-auto text-white">
-            <div class="col-span-6 flex text-center" >
+            <div class="col-span-6 flex text-center">
                 <img src="~assets/images/logo.png" class="overflow-hidden object-cover h-38 w-60" alt="">
             </div>
             <div class="col-span-3">
                 <div class="ms-auto text-white">
-                    <a to="/">Home </a>
-                    <a to="/about">About </a>
-                    <a to="/products">Products</a>
-                    <a to="/products/byname">Contact</a>
-                    <a to="/products/byname">
-                      <SfIconSearch />Search
-                    </a>
-                  </div>
+                    <ol>
+                        <li>
+                            <NuxtLink to="/">Home </NuxtLink>
+                            <NuxtLink to="/about">About </NuxtLink>
+                            <NuxtLink to="/products">Products</NuxtLink>
+                            <NuxtLink to="/products/byname">Contact</NuxtLink>
+                            <NuxtLink to="/products/byname">
+                                <SfIconSearch />Search
+                            </NuxtLink>
+                        </li>
+                    </ol>
+                </div>
             </div>
-            <div class="col-span-2 text-center"> 
+            <div class="col-span-2 text-center">
                 <h1 class="text-lg">We Accept</h1>
                 <ol>
                     <li><a href="">Esewa</a></li>
@@ -26,12 +30,12 @@
             </div>
             <div class="col-span-4 text-center py-5">
                 <h1 class="text-lg">2023 The Nepalese Kitchen<br>
-                Proudly built by Anjila_Tg</h1>
+                    Proudly built by Anjila_Tg</h1>
             </div>
 
         </div>
     </div>
-    
+
 </template>
 
 <script setup>
